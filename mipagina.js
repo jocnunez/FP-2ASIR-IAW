0c9124de228baf163c220cb0fa57fd42528c78ed
Fabrizio Edu Trujillo Begazo <149820772+Darksite2002@users.noreply.github.com>
2023-11-30 18:24:04 +0000

    viernes pasado

--- mipagina.js
+++ mipagina.js
@@ -1,25 +1,10 @@
-console.log("Hola mundo")
+import {secundaria} from './practica1.js'
 
-document.addEventListener("DOMContentLoaded", init)
+let principal = 100
 
-let msg = "Hola mundo"
-
-function init(event) {
-    let title = document.getElementById("title")
-    console.log(title)
-    title.style.color = "red"
-    title.style.backgroundColor = "blue"
-    title.innerHTML = msg
-    loadData("data.json")
-}
-
-function loadData(url) {
-    let json 
-    fetch(url)
-        .then(response => response.json())
-        .then(data => Hobbies(data))
-}
-
-function Hobbies(json) {
-    console.log(json)
+document.getElementById("botonEnviar").onclick =
+function saludar() {
+    let resultado = document.getElementById("resultado")
+    let nombre = document.getElementById("campoNombre")
+    resultado.innerHTML = nombre.value
 }
---
+++ practica1.js
@@ -0,0 +1,2 @@
+export let secundaria = 50
+
