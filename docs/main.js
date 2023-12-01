bc7af5c32d43d46dc0b8f036bf0ab5e2aaab9ed1
jocnunez <jose.nunnez@gmail.com>
2023-12-01 17:00:20 +0100

    add practices web

---
+++ docs/main.js
@@ -0,0 +1,6 @@
+{
+    function toggleFolded(event) {
+        parent = event.target.parentElement;
+        parent.classList.toggle("folded");
+    }
+}
---
+++ docs/practicas/practica1.js
@@ -0,0 +1,14 @@
+//En cada ejercicio crearemos un nuevo bloque (con { y }) donde escribiremos nuestro código
+//Así podemos usar variables con el mismo nombre en diferentes ejercicios sin que haya ningún conflicto
+
+//Ejercicio 1
+{
+    //Borra este comentario y escribe aquí el código necesario para el ejercicio 1
+    console.log("esto es una prueba");
+}
+
+//Ejercicio 2
+{
+}
+
+//Ejercicio 3 ...
