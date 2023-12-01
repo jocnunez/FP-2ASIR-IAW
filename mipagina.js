74ef2410b1a232aa7332d61974641c5cf1677075
Fabrizio Edu Trujillo Begazo <149820772+Darksite2002@users.noreply.github.com>
2023-12-01 17:30:04 +0000

    hola

--- mipagina.js
+++ mipagina.js
@@ -8,11 +8,23 @@
 ///}
 
 function factorial(n) {
-    let i = 1
+    let i = n
     let resol = 1
-    while (i <= numero) {
-        let resol = resol * i
-        let i = i + 1
+    while (i >= 1) {
+        resol = resol * i
+        i = i - 1
     }
     return resol
-};
+}
+
+function suma(lista) {
+    let resultado = 0;
+    for (let i = 0; i < lista.lenght; i = i + 1) {
+        resultado = resultado + lista[i]
+    }
+    return resultado;
+}
+
+let numeros = [120,200,5,48,190]
+
+console.log(suma(numeros))
